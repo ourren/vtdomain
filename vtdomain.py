0ab268eda26db33748c81db358ac98c7c7f7b45d
ourren <ourren@outlook.com>
2015-05-14 23:11:14 -0700

    add multi api key

--- vtdomain.py
+++ vtdomain.py
@@ -11,7 +11,7 @@ import sqlite3
 import time
 
 uv = "https://www.virustotal.com/vtapi/v2/url/report"
-apikey = "add you own api"
+apikey = ["add api key 1", "add api key 2", "add api key 3"]
 
 db_eml = 'm_domain.db'
 db = sqlite3.connect(db_eml)
@@ -26,48 +26,51 @@ def table_mdomain():
     c.execute("""create table if not exists domain( id integer primary key autoincrement, domain text, positive text, AutoShun text, Quttera text, ADMINUSLabs text, AegisLab text, AlienVault text, Antiy text, Avira text, Baidu text, BitDefender text, Blueliv text, CSIRT text, CLEAN text, CRDF text, Comodo text, CyberCrime text, DrWeb text, ESET text, Emsisoft text, Fortinet text, FraudSense text, GData text, Google text, K7AntiVirus text, Kaspersky text, Malc0de text, Malekal text, Malware text, MalwareDomainList text, MalwarePatrol text, Malwarebytes text, Malwared text, Netcraft text, OpenPhish text, Opera text, PalevoTracker text, ParetoLogic text, PhishLabs text, Phishtank text, Rising text, SCUMWARE text, SecureBrain text, Sophos text, Spam404 text, SpyEyeTracker text, StopBadware text, Sucuri text, Tencent text, ThreatHive text, Trustwave text, URLQuery text, VXVault text, WebSecurityGuard text, Websense text, Webutation text, Wepawet text, Yandex text, ZCloudsec text, ZDB text, ZeroCERT text, Zerofox text, ZeusTracker text, malwares text, zvelo text) """)
     db.commit()
 
-def check(url):
-    # remove http://
-    if url.startswith("http://") or url.startswith("https://"):
-        url = url.replace("http://", "").replace("https://", "")
-    # check if exist
-    c = db.cursor()
-    c.execute("select count(*) from domain where domain ='" + url + "'")
-    row = c.fetchone()
-    if row[0] == 0:
-        parameters = {"resource": url, "apikey": apikey}
-        data = urllib.urlencode(parameters)
-        try:
-            req = urllib2.Request(uv, data)
-            response = urllib2.urlopen(req)
-            json = response.read()
-            response_dict = simplejson.loads(json)
-            positives = response_dict.get("positives")
-            print url, positives
-            c.execute("""insert into domain(domain, positive) values(?,?)""", (url, positives))
-            db.commit()
-            id = c.lastrowid
-            scans = response_dict["scans"]
-            for (k, v) in vtdic.items():
-                usql = "UPDATE domain set " + v + " = '" + scans[k]["result"] + "' where id="+ str(id)
-                c.execute(usql)
-            db.commit()
-            return True
-        except:
-            return True
-    else:
-        print 'already checked..'
-        return False
-
-
-def main():
-    print '[*] App: vtdomain check'
-    print '[*] Version: V1.1(20150514)'
-    table_mdomain()
-    for line in open("domain").readlines():
-        line = line.strip()
-        if check(line):
-            time.sleep(15)
+def check(file):
+    i = 0
+    for url in open("domain").readlines():
+        i += 1
+        url = url.strip()
+        # remove http://
+        if url.startswith("http://") or url.startswith("https://"):
+            url = url.replace("http://", "").replace("https://", "")
+        # check if exist
+        c = db.cursor()
+        c.execute("select count(*) from domain where domain ='" + url + "'")
+        row = c.fetchone()
+        if row[0] == 0:
+            key = i%len(apikey)
+            print apikey[key]
+            parameters = {"resource": url, "apikey": apikey[key]}
+            data = urllib.urlencode(parameters)
+            try:
+                req = urllib2.Request(uv, data)
+                response = urllib2.urlopen(req)
+                json = response.read()
+                response_dict = simplejson.loads(json)
+                positives = response_dict.get("positives")
+                print url, positives
+                c.execute("""insert into domain(domain, positive) values(?,?)""", (url, positives))
+                db.commit()
+                id = c.lastrowid
+                scans = response_dict["scans"]
+                for (k, v) in vtdic.items():
+                    try:
+                        usql = "UPDATE domain set " + v + " = '" + scans[k]["result"] + "' where id="+ str(id)
+                        c.execute(usql)
+                    except:
+                        continue
+                db.commit()
+            except:
+                db.commit()
+            time.sleep(15/len(apikey))
+        else:
+            print 'already checked..'
 
 if __name__ == '__main__':
-    main()
+    print '[*] App: vtdomain check'
+    print '[*] Version: V1.0(20150503)'
+    table_mdomain()
+    # your domain file
+    check('domain')
+
